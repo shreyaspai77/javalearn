c4ac61da42eb8960b3c61c951643d5839e5f9459
shreyaspai77 <shreyaspai25@gmail.com>
2023-02-27 01:01:52 +0530

    created a random number array-generator

---
+++ searching/binary/binarysearch.java
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/binary-search/solutions/
+
+import java.lang.annotation.Target;
+import java.util.Arrays;
+import java.util.Random;
+
+public class binarysearch {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(101);
+        }
+        Arrays.sort(arr);
+        int Target = random.nextInt(101);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Target:" + Target);
+        int index = search(arr, Target);
+        System.out.println("the index of the element is "+index);
+    }
+
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+            mid = (low + high) / 2;
+        }
+        return -1;
+    }
+
+}
