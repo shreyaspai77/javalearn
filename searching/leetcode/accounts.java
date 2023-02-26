b17e0d9cc2c49411f61844969d234297273f1810
shreyaspai77 <shreyaspai25@gmail.com>
2023-02-26 23:36:10 +0530

    changed a few things about the structure

---
+++ searching/leetcode/accounts.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/richest-customer-wealth/
+package searching.leetcode;
+
+class accounts{
+    public int maximumWealth(int[][] accounts) {
+        // traverse
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+        for (int[] person : accounts) {
+            for (int account : person) {
+                sum += account;
+            }
+            if (sum > ans) {
+                ans = sum;
+            }
+            sum = 0;
+        }
+        return ans;
+    }
+}
